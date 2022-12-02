9547b4ab51b4df072bac85b1ca022060e871ce4c
otheobaldo <102699073+otheobaldo@users.noreply.github.com>
2022-12-02 15:14:56 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,33 +1,35 @@
+var names = {'A Viagem de Chihiro' : 'a-viagem-de-chihiro.html', 'Vidas ao Vento' : 'vidas-ao-vento.html',  'O Conto da Princesa Kaguya' : 'o-conto-da-princesa-kaguya.html',  'Da Colina Kokuriko' : 'da-colina-kokuriko.html', 'As Memórias de Marnie' : 'as-memorias-de-marnie.html', 'Meu Amigo Totoro' : 'meu-amigo-totoro.html',  'O Castelo Animado' : 'o-castelo-animado.html', 'O Mundo dos Pequeninos' : 'o-mundo-dos-pequeninos.html',  'Eu Posso Ouvir o Oceano' : 'eu-posso-ouvir-o-oceano.html',  'O Reino dos Gatos' : 'o-reino-dos-gatos.html',  'Contos de Terramar' : 'contos-de-terramar.html',  'Ponyo: Uma Amizade que Veio do Mar' : 'ponyo.html',  'O Castelo no Céu' : 'o-castelo-no-ceu.html',  'Sussurros do Coração' : 'sussurros-do-coracao.html',  'Princesa Mononoke' : 'princesa-mononoke.html',  'Pom Poko: A Grande Batalha dos Guaxinins' : 'pompoko.html',  'Porco Rosso: O Último Herói Romântico' : 'porco-rosso.html',  'Memórias de Ontem' : 'memorias-de-ontem.html',  'O Serviço de Entregas da Kiki' : 'o-servico-de-entregas-da-kiki.html',  'Nausicaä do Vale do Vento' : 'nausicaa-do-vale-do-vento.html',  'O Túmulo dos Vagalumes' : 'o-tumulo-dos-vagalumes.html',  'Meus vizinhos, Os Yamadas' : 'meus-vizinhos.html',  'Aya e a Bruxa' : 'aya-e-a-bruxa.html'};
+
 // Botao Scroll-up
 
-    var btn = document.querySelector("#scroll-up");
-    btn.addEventListener("click", function() {
-        window.scrollTo(0, 0);
-    });
+var tmp = document.querySelector("#scroll-up");
+tmp.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
 
 // Pesquisar
 
-    function element(id) {
-        return document.getElementById(id);
-    }
+function element(id) {
+    return document.getElementById(id);
+}
 
-    let allSearchData = "";
+let allSearchData = "";
 
-    function getResults() {
+function getResults() {
 
-        let search = element("search").value;
-        allSearchData = "";
+    let search = element("search").value;
+    allSearchData = "";
 
-        hideSearchResults();
-        clearSearchResults();
-        clearSearchData();
+    hideSearchResults();
+    clearSearchResults();
+    clearSearchData();
 
-        if (search.length > 1) {
-            let counter = 0;
-            for (let x in names) {
+    if (search.length > 1) {
+        let counter = 0; 
+        for (let x in names) {
             if (counter < 10) {
                 if (x.toLowerCase().includes(search.toLowerCase())) {
-                    element("search-results").innerHTML += "<div class='search-item' onclick='displayData(\"" + x +"\")'><p>" +x +"</p></div>";
+                    element("search-results").innerHTML += "<div class='search-item' onekeydown onclick='displayData(\"" + x +"\")'><p>" +x +"</p></div>";
                     counter++;
                 }
             }
@@ -36,50 +38,43 @@
             }
             displaySearchResults();
         }
-        }
-
-    function displaySearchResults() {
-        element("search-results").style.display = "block";
     }
 
-    function clearSearchResults() {
-        element("search-results").innerHTML = "";
+function displaySearchResults() {
+    element("search-results").style.display = "block";
+}
+
+function clearSearchResults() {
+    element("search-results").innerHTML = "";
+}
+
+function hideSearchResults() {
+    element("search-results").style.display = "none";
+}
+
+function displayData(name) {
+    if (document.title == "Susuwatari"){
+        window.open("html/"+names[name], "_self");
     }
+    else{
+        window.open("../html/"+names[name], "_self");
+    }        
+    hideSearchResults();
+}
 
-    function hideSearchResults() {
-        element("search-results").style.display = "none";
-    }
+function displayAllData(names) {
+    element("search-data").innerHTML = names;
+    hideSearchResults();
+}
 
-    function displayData(name) {
-        if (document.title == "Susuwatari"){
-            window.open("html/"+names[name], "mozillaTab");
-        }
-        else{
-            window.open("../html/"+names[name], "mozillaTab");
-        }        
-        hideSearchResults();
-    }
+function clearSearchData() {
+    element("search-data").innerHTML = "";
+}
 
-    function displayAllData(names) {
-        element("search-data").innerHTML = names;
-        hideSearchResults();
-    }
+element("search").oninput = function() {
+    getResults();
+};
 
-    function clearSearchData() {
-        element("search-data").innerHTML = "";
-    }
-
-    element("search").oninput = function() {
-        getResults();
-    };
-
-    element("search").addEventListener("keyup", function(event) {
-        // Number 13 is the "Enter" key on the keyboard
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            displayAllData(allSearchData);
-        }
-    });
 
 // Botão para desfocar
 
@@ -87,21 +82,14 @@ function desfocus() {
     document.getElementById("search").value = "";
 }
 
-// Alto contraste
+document.querySelector('#carousel-1').addEventListener('click', () => {
+  window.open("html/sobre.html","_self")
+})
 
-const tmp = document.getElementById("alto-contraste");
-const lnk = document.querySelector("link[href='../filmes.css']");
-const stlcss = new URL("css/filmes.css", window.location)
+document.querySelector('#carousel-2').addEventListener('click', () => {
+  window.open("html/ponyo.html","_self")
+})
 
-function aa() {
-    if (lnk.href == stlcss.href){
-        lnk.href = "css/c.css"; 
-    } 
-    else {
-        lnk.href = "css/filmes.css";
-    }
-}
-
-
-
-
+document.querySelector('#carousel-3').addEventListener('click', () => {
+  window.open("html/o-servico-de-entregas-da-kiki.html","_self");
+})
